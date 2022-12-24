848c8f935ffd3e1ef8c795d84e427bf28039caf3
Da-Real-Kryall <codyryall05@gmail.com>
2022-12-24 15:38:59 +0800

    Fixed status going blank

--- Helpercode.py
+++ Helpercode.py
@@ -30,10 +30,10 @@ async def on_connect():
     Bot.lavalink.add_node('127.0.0.1', 2333, 'youshallnotpass', 'au', 'default-node')
     # ansi escape code for green: 
     print("\x1b[32m I'm ready!\x1b[0m")
-    await Bot.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name=f" {str(len(Bot.guilds))} servers | @{str(Bot.user.name)} for help."))
-    #while 1:
-    #    await Bot.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name=f" {str(len(Bot.guilds))} servers | @{str(Bot.user.name)} for help."))
-    #    await asyncio.sleep(60)
+    #await Bot.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name=f" {str(len(Bot.guilds))} servers | @{str(Bot.user.name)} for help."))
+    while 1:
+        await Bot.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name=f" {str(len(Bot.guilds))} servers | @{str(Bot.user.name)} for help."))
+        await asyncio.sleep(60)
     #    await Bot.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name=f"REWRITE COMPLETED!"))
     #    await asyncio.sleep(15)
 
